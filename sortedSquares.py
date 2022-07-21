ccb0cacd28b02b6568d4f3365bd5e3d8993d5b36
windoseptiawan <57704810+windoseptiawan@users.noreply.github.com>
2022-07-21 13:30:43 +0700

    Update sortedSquares.py

--- sortedSquares.py
+++ sortedSquares.py
@@ -6,5 +6,12 @@ class Solution(object):
         for num in nums:
             res.append(num**2)
         return sorted(res)
+
+class Solution(object):
+    def sortedSquares(self, nums):
+        
+        return sorted(num**2 for num in nums)
+       
+
         
        
